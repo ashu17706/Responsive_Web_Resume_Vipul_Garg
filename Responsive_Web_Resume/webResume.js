3546615f834b68dbc2007adafaf3572fedafb27b
Vipul Garg <vipul4017@users.noreply.github.com>
2017-09-11 16:06:43 +0530

    ResponsiveResume_Vipul
    GrabO  Assignment Responsive Resume

---
+++ Responsive_Web_Resume/webResume.js
@@ -0,0 +1,16 @@
+
+// Function for showing Map location
+
+function initMap() {
+var mapProp= {
+    center:new google.maps.LatLng(26.8634, 75.7776),
+    zoom:12,
+};
+var map=new google.maps.Map(document.getElementById("map"),mapProp);
+}
+
+
+
+
+    
+
